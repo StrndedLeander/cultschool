b574b7c3fcdabe9776f09e3787a1744b27ef8d63
StrndedLeander <strnded.vandiepen@gmail.com>
2019-05-22 15:10:03 +0200

    finished navbar desgin and toggling burger

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,13 +2,26 @@
   <div class="navigation hero is-desktop">
     <nav class="navbar has-text-centered" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <router-link>
+        <router-link class="profile" to="#">
           <span class="icon">
             <i class="fas fa-user"></i>
           </span>
         </router-link>
+        <StudentsExp class="studentsExp"></StudentsExp>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbars"
+          @click="toggleMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div class="navbar-menu">
+      <div class="navbar-menu" id="navbars">
         <div class="navbar-start">
           <a class="nav-item classes">
             <p>CLASSES</p>
@@ -32,64 +45,95 @@
             <p>EXPLORE</p>
           </a>
         </div>
-        <div class="navbar-end"></div>
+        <div class="navbar-end">
+          <a class="navEnd">
+            <span class="icon">
+              <i class="fas fa-search"></i>
+            </span>
+          </a>
+          <a class="navEnd">
+            <span class="icon">
+              <i class="fas fa-bell"></i>
+            </span>
+          </a>
+          <a class="navEnd">
+            <span class="icon">
+              <i class="fas fa-heart"></i>
+            </span>
+          </a>
+        </div>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
-export default {};
+import StudentsExp from "./Students/Experience";
+export default {
+  components: {
+    StudentsExp
+  },
+  methods: {
+    toggleMenu(e) {
+      e.target.classList.toggle("is-active");
+
+      const target = e.target.dataset.target;
+      console.log(target);
+      const $target = document.getElementById(target);
+
+      $target.classList.toggle("is-active");
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.navbar {
-  width: 100%;
-}
-.navbar-brand {
-  width: 25%;
-  border: 1px solid black;
-}
-.navbar-start {
-  width: 66.67%;
-  border: 1px solid black;
-}
-.navbar-end {
-  width: 33.3%;
-  border: 1px solid black;
-}
-.classes{
-  margin-left: 3%;
-}
-.courses{
-  margin-left: 10%;
-}
-a {
-  margin-right: 15%;
-}
-a p {
-  margin-top: 25%;
-}
 .navigation {
   background-color: #bbbbbb;
   width: 100%;
   height: 70px;
 }
-.column {
-  border: 1px solid black;
-}
 .navbar {
+  width: 100%;
   height: 70px;
 }
-a {
+.navbar-brand {
+  width: 25%;
+}
+.navbar-start {
+  width: 67%;
+}
+.navbar-end {
+  width: 33%;
+}
+.nav-item {
+  width: 25%;
+  margin-right: 15%;
   color: #667581;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 15px;
 }
+.classes {
+  margin-left: 3%;
+}
+.courses {
+  margin-left: 10%;
+}
+a p {
+  margin-top: 25%;
+}
+.profile {
+  margin-left: 1rem;
+  margin-top: 1.5rem;
+  margin-right: 0.5rem;
+  color: black;
+}
+.navEnd {
+  color: black;
+  margin-top: 1.5rem;
+  margin-right: 0.5rem;
+}
 .fa-chevron-down {
   color: #f5f5f5;
-}
-.studentExp {
-  border: 1px solid black;
 }
 </style>
---
+++ client/src/components/Students/Experience.vue
@@ -0,0 +1,41 @@
+<template>
+  <radial-progress-bar class="progress"
+    :diameter="50"
+    :completed-steps="studentLevel"
+    :total-steps="totalLevel"
+    :strokeWidth="6"
+    :startColor=startColor
+    :innerStrokeColor=innerStrokeColor
+  >
+    <p>Ø {{ studentLevel }}</p>
+  </radial-progress-bar>
+</template>
+
+<script>
+import RadialProgressBar from "vue-radial-progress";
+
+export default {
+  data() {
+    return {
+      studentLevel: 5,
+      totalLevel: 100,
+      startColor: '#B553FF',
+      innerStrokeColor: '#73818B'
+    };
+  },
+
+  components: {
+    RadialProgressBar
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.progress {
+    margin-top:0.5rem;
+}
+p {
+  font-size: 11px;
+  font-weight: 600;
+}
+</style>
